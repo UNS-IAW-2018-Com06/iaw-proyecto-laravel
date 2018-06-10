5e82ea4a69b4ec6d20369dcc5e8264b6d1c71e8d
DESKTOP-5F1ACM6\monica <leandro18195@hotmail.com>
2018-06-10 01:21:59 -0300

    visualizar carreras..

--- resources/views/universidadindex.blade.php
+++ resources/views/universidadindex.blade.php
@@ -31,25 +31,38 @@
       </tr>
     </thead>
     <tbody> 
-      @foreach($uni as $universidad)
-        {{$universidad->nombre}}<br>
-        {{$universidad->ciudad}}<br>
-        {{$universidad->provincia}}<br>
-        {{$universidad->dirección}}<br>
-        @foreach($universidad->coordenadas as $coord)
-           {{$coord}}<br>
-
-        @endforeach
-
+       @foreach($universidades as $universidad)
+      <tr>
+        <td>{{$universidad->nombre}}</td>
+        <td>{{$universidad->provincia}}</td>
+        <td>{{$universidad->ciudad}}</td>
+        <td>{{$universidad->direccion}}</td>
+        @if(!empty($universidad->telefono))
+        <td>{{$universidad->telefono}}</td>
+        @endif
+        @if(!empty($universidad->web))
+        <td>{{$universidad->web}}</td>  
+        @endif       
+        <td>{{$universidad->coordenadas[0]}}</td>      
+        <td>{{$universidad->coordenadas[1]}}</td> 
         @if(!empty($universidad->carreras_grado))
-            @foreach($universidad->carreras_grado as $c)
-              {{$c->nombre_carrera}}<br>
-
+             @foreach($universidad->carreras_grado as $carrera)
+        <td> 
+          <select> <option>  {{$carrera->nombre_carrera}} </option> </select>
+        </td> 
             @endforeach
         @endif
-      @endforeach
-      </tbody>
-    </table>
+        
+        <td><a href="{{action('UniversidadController@edit', $universidad->id)}}" class="btn btn-warning">Edit</a></td>
+        <td>
+          <form action="{{action('UniversidadController@destroy', $universidad->id)}}" method="post">
+            @csrf
+            <input name="_method" type="hidden" value="DELETE">
+            <button class="btn btn-danger" type="submit">Delete</button>
+          </form>
+        </td>
+      </tr>
+       @endforeach
       <tr>
         <a href="{{action('UniversidadController@store')}}" class="btn btn-warning">Agregar</a>
       </tr>
